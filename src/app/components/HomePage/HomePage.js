755e6fa6fe527282c3e677a71e4086e64677ec49
Trần Tuấn Anh <tuananhtran22052000@gmail.com>
2023-03-24 00:07:50 +0700

    update project

---
+++ src/app/components/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classNames from './HomePage.module.scss'
+
+const HomePage = () =>{
+return(
+    <div className={classNames.container}></div>
+)
+}
+export default HomePage
